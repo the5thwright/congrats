7b0bceeed9484765ca13bf25bd37f8f0a513926e
the5thwright <awright5th@gmail.com>
2025-04-29 07:17:30 -0400

    Update notepad_automation.py with working Python code to open Notepad.

--- notepad_automation.py
+++ notepad_automation.py
@@ -1,16 +1,4 @@
-import pyautogui
-import time
 import subprocess
 
-def write_love_message():
-    # Open Notepad
-    subprocess.Popen('notepad.exe')
-
-    # Wait for Notepad to open
-    time.sleep(1)
-
-    # Type the message
-    pyautogui.write('I love you')
-
-if __name__ == '__main__':
-    write_love_message()
+# This script opens Notepad on Windows
+subprocess.Popen(["notepad.exe"])
